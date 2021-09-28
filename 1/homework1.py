df86460445530e8acbd246d118a5a56b44b39e2f
true42 <bezmaternykh.alexey@gmail.com>
2021-09-28 20:27:36 +0300

    homework1 done

---
+++ 1/homework1.py
@@ -0,0 +1,42 @@
+import requests
+from pprint import pprint
+import json
+
+'''
+1. Посмотреть документацию к API GitHub, разобраться как вывести список репозиториев для конкретного пользователя, 
+сохранить JSON-вывод в файле *.json.
+'''
+
+GIT_API_URL = 'https://api.github.com'
+USER = 'true42'
+
+response = requests.get(f'{GIT_API_URL}/users/{USER}/repos')
+j_data = response.json()
+
+with open('repos.json', 'w') as f:
+    f.write(json.dumps(j_data, sort_keys=True, indent=4, separators=(',', ': ')))
+
+'''
+2. Изучить список открытых API. Найти среди них любое, требующее авторизацию (любого типа). 
+Выполнить запросы к нему, пройдя авторизацию. 
+Ответ сервера записать в файл.
+'''
+
+VK_API_URL = 'https://api.vk.com/method/'
+METHOD = 'users.get'
+TOKEN = ''
+V = '5.131'
+
+user_ids = 'true42_ab'
+fields = 'online'
+PARAMS = {'user_ids': user_ids,
+          'fields': fields,
+          'access_token': TOKEN,
+          'v': V
+          }
+
+x = requests.get(VK_API_URL + METHOD, params=PARAMS)
+
+j_data2 = x.json()
+
+pprint(j_data2)
